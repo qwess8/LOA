9ae47bd300164805a24705e3cdf40b8d7115fcbe
qwess8 <66124997+qwess8@users.noreply.github.com>
2024-09-13 15:08:14 +0300

    task3

--- Labor1.cpp
+++ Labor1.cpp
@@ -7,24 +7,28 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int num[10];
+	int *num, n;
 	int min, max;
 	srand(time(NULL));
-	
-	for(int i = 0; i<10; i++){
+	scanf("%d", &n);
+	num =(int*)malloc(n * sizeof(int));
+	for(int i = 0; i<n; i++){
 		num[i] = rand()%10;
 		printf("%d\n", num[i]);
 	}
 
 	min = num[0];
 	max= num[0];
-	for(int i = 0; i < 10; i++){
+	for(int i = 0; i < n; i++){
 		if(num[i]< min) min = num[i];
 		if(num[i]> max) max = num[i];
 	}
 
 	printf("\nmax - min = %d", max-min);
+	free(num);
 	
+
+	getchar();
 	getchar();
 	return 0;
 }
