0621c5d5c04942729b656588565a7bfdd1204d57
qwess8 <66124997+qwess8@users.noreply.github.com>
2024-09-13 16:21:03 +0300

    task5

--- Labor1.cpp
+++ Labor1.cpp
@@ -1,31 +1,54 @@
-// Labor1.cpp: определяет точку входа для консольного приложения.
+// labor1.cpp: определяет точку входа для консольного приложения.
 //
 
 #include "stdafx.h"
 #include <time.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int num[10][10];
-	int sum = 0;
-	srand(time(NULL));
-	for(int i = 0; i<10; i++){
-		for(int j = 0; j<10; j++){
-			num[i][j] = rand()%10;
-			printf(" %d ", num[i][j]);
-		}
-		printf("\n");
+	setlocale(LC_ALL, "Rus");
+	struct student
+	{
+		char famil[20];
+		char name[20], facult[20];
+		int Nomzach;
+	} stud[3];
+
+	char imya[20], familiya[20];
+
+	for(int i=0; i<3; i++){
+		printf("Введите фамилию студента\n"); scanf("%20s", stud[i].famil);
 	}
-	printf("\n");
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			sum+=num[j][i];
+
+	for(int i=0; i<3; i++){
+		printf("Введите имя студента\n"); scanf("%20s", stud[i].name);
+	}
+
+	for(int i=0; i<3; i++){
+		printf("Введите название факультета студента %s %s\n", stud[i].famil, stud[i].name); scanf("%20s", stud[i].facult);
+	}
+
+	for(int i=0; i<3; i++){
+		printf("Введите номер зачетной книжки студента %s %s\n", stud[i].famil, stud[i].name); scanf("%d", &stud[i].Nomzach);
+	}
+
+	for(int i=0; i<3; i++){
+		printf("Студент %s %s обучается на факультете %s, номер зачетной книжки %d\n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].Nomzach);
+	}
+
+	printf("Введите имя нужного студента: ");scanf("%20s", imya);
+	printf("Введите фамилию нужного студента: ");scanf("%20s", familiya);
+
+	for(int i=0; i<3; i++){
+		if(strcmp(stud[i].name, imya)==0 && strcmp(stud[i].famil, familiya)==0){
+			printf("Студент %s %s обучается на факультете %s, номер зачетной книжки %d\n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].Nomzach);
 		}
-		printf("%d ", sum);
-		sum = 0;
 	}
 	
+	getchar();
 	getchar();
 	return 0;
 }
